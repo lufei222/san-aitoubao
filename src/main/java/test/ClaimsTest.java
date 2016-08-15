a04b4381f1476d97ec166b83f6f7f180ed9f4514
luosan <1132647424@qq.com>
2016-08-15 16:58:24 +0800

    java对象值引用传递/这就是String的“类似值传递”(即可以将String看做int,double等基本类型，这些基本类型作为参数是值传递，但String在java规范中又不是基本类型，所以此处称其为“类似值传递”

--- src/main/java/test/ClaimsTest.java
+++ src/main/java/test/ClaimsTest.java
@@ -17,46 +17,50 @@ import java.util.Map;
  * Created by san-pc on 2016/3/2.
  */
 public class ClaimsTest {
+    private static class Person{
+        Integer age;
+        String name;
 
-//    @Autowired
-//    public static ClaimService claimService;
+        public Integer getAge() {
+            return age;
+        }
 
-    @BeforeClass
-    public static void init() {//junit之前init spring
-        ApplicationContext context = new ClassPathXmlApplicationContext("classpath*:conf/root.xml");//
-    //    claimService = (ClaimService) context.getBean("claimService");
+        public void setAge(Integer age) {
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
+    public static void main(String[] args){
 
-    @Test
-    public void addClaim() throws Exception {
-//        //  for(int i=0;i<2;i++) {
-//        Claim claim = new Claim();
-//        //claim.setCode("C2016032703" + i);
-//        claim.setCode("C2016032703");
-//        claim.setClaimsType(1);
-//        claim.setApplicantId(16292);
-//        claim.setApplicant("胡安");
-//        claim.setStateCode("1");
-//        claim.setFillerId(16224);
-//        claim.setFillerName("胡柳茵");
-//        claim.setStartDate(new Date());//新建日期
-//        claim.setEditDate(new Date());//最后修改时间
-//        claim.setEndDate(new Date());//完结时间
-//        claim.setRemark("fed");
-//         int claimId = 2408 ;
-//        claim.setId(2407);
-//        PagingBean<Claim> pb = new PagingBean<Claim>();
-//        pb.setCondition(claim);
-//     //   claimService.insertEntity(claim);//增加
-//   //     claimService.updateEntity(claim);//修改
-//        claimService.selectEntityById(claimId);//查询单条
-//       // claimService.selectAllEntities(pb);//查询全部，
-//        //claimService.selectEntities(pb);//分页查询，
-//        //claimService.selectEntitiesCount(pb);//查询条数，
-//        claimService.deleteEntity(claimId);//删除
-//
-//        System.out.println();
-        //   }
+        ClaimsTest test = new ClaimsTest();
+        String s = new String("hello");
+        Integer a=new Integer(3);
+        test.notice(s);
+        System.out.println("String=" + s);
+        System.out.println("Integer=" + a);
+        Person person=new Person();
+        person.setName("我是大明");
+        test.changeName(person);
+        System.out.println("person.name="+person.getName());
+
     }
-
+    public static void notice(String str){
+       // str = str.replace('h', 'x');
+        str="diaomao";
+    }
+    public static void changeInt(Integer i){
+        // str = str.replace('h', 'x');
+        i=9;
+    }
+    public static void changeName(Person person){
+        // str = str.replace('h', 'x');
+        person.setName("我是可爱的小明");
+    }
 }
